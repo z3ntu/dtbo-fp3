20324c21169622e7c68eb36524c74fb1da8837d5
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-12-18 20:33:30 -0800

    build dtbo partition

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Minimal dtbo partition for Google Pixel 3 XL (google-crosshatch), taken from the AOSP kernel with all device nodes removed.
 
 Used for mainline kernel.
+
+This requires dtc and mkdtboimg (from AOSP libufdt) to build.
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 # https://learn.adafruit.com/introduction-to-the-beaglebone-black-device-tree/compiling-an-overlay
-dtc -O dtb -o sdm845-c1-dvt1.1.dtb -b 0 -@ sdm845-c1-dvt1.1.dts
+dtc -O dtb -o sdm845-c1-dvt1.1.dtbo -b 0 -@ sdm845-c1-dvt1.1.dts
+mkdtboimg cfg_create dtbo.img dtboimg.cfg
